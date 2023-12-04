2cf4a4337b2b4eca725dae6494e43c9aaabbd1fb
Luana Paes <luanafranca598@gmail.com>
2023-12-03 23:46:16 -0300

    at

--- API_CSV/myproject/myapp/urls.py
+++ API_CSV/myproject/myapp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 from django.contrib import admin
+from django.urls import path
+from myapp.views import MinhaModelListCreateView
 
 urlpatterns = [
+    # ... outras configurações de URL ...
     path('admin/', admin.site.urls),
     path('meu_app/', include('myproject.urls')),
-    path('app_flask/', include('meu_app_flask.urls')),
+    path('api/minhamodel/', MinhaModelListCreateView.as_view(), name='minhamodel-list-create'),
+    # ... outras configurações de URL ...
 ]
--- API_CSV/myproject/myproject/urls.py
+++ API_CSV/myproject/myproject/urls.py
@@ -5,5 +5,6 @@ from myapp import views
 urlpatterns = [
     path('', views.upload_csv, name='upload_csv'),
     path('api/minhamodel/',views.MinhaModelListCreateView.as_view(),name='minhamodel-list-create'),
+      path('usuario/', views.usuario_previsao, name='usuario')
 
 ]
