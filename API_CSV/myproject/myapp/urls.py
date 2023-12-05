15abd5903e42ea0320cfebfe8e1ecbdd0db2cd2a
Luana Paes <luanafranca598@gmail.com>
2023-12-05 00:23:24 -0300

    integrando a ia ao código
    com as alterações feitas, é possível preencher a coluna attended com a previsão gerada pela ia.

--- API_CSV/myproject/myapp/urls.py
+++ API_CSV/myproject/myapp/urls.py
@@ -2,11 +2,13 @@ from django.urls import path, include
 from django.contrib import admin
 from django.urls import path
 from myapp.views import MinhaModelListCreateView
+from myapp.views import BaseComPrecisaoListCreateView
+
 
 urlpatterns = [
-    # ... outras configurações de URL ...
     path('admin/', admin.site.urls),
     path('meu_app/', include('myproject.urls')),
     path('api/minhamodel/', MinhaModelListCreateView.as_view(), name='minhamodel-list-create'),
-    # ... outras configurações de URL ...
+    path('api/basecomprevisao/', BaseComPrecisaoListCreateView,
+         name='base-com-previsao-list-create'),
 ]
